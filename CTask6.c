d4eb9ad3504ae53e78d137d8a3c9411bf4030330
Muhammad Shuraim <k250733@nu.edu.pk>
2025-10-01 21:53:35 +0500

    Update CTask6.c

--- CTask6.c
+++ CTask6.c
@@ -1,15 +1,24 @@
-#include<stdio.h>
-int main(){
-	int original = 1234;
-	int user;
-	
-	
-	do{
-		printf("Enter the password: ");
-		scanf("%d" , &user);
-		
-	} while (user != original);
-	
-	printf("Access granted");
-	return 0;
+#include <stdio.h>
+
+int main() {
+    int i, sum = 0, oddSum = 0, n;
+
+    printf("Enter the range of number(1-20): ");
+    scanf("%d", &n);
+
+    for(i = 2; i <= n; i++) {
+        if(i % 2 == 0)
+            sum = sum + i;
+    }
+
+    for(i = 1; i <= n; i++) {
+        if(i % 2 != 0)
+            oddSum = oddSum + i;
+    }
+
+    printf("The sum of even number from 1 to %d: %d\n", n, sum);
+    printf("The sum of odd number from 1 to %d: %d\n", n, oddSum);
+
+    return 0;
 }
+
