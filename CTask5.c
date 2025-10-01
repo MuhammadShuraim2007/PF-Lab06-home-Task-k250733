42134fd36ff2608645312342082f234838556dff
Muhammad Shuraim <k250733@nu.edu.pk>
2025-10-01 22:01:01 +0500

    Update CTask5.c

--- CTask5.c
+++ CTask5.c
@@ -1,15 +1,24 @@
-#include<stdio.h>
-int main(){
-	int original = 1234;
-	int user;
-	
-	
-	do{
-		printf("Enter the password: ");
-		scanf("%d" , &user);
-		
-	} while (user != original);
-	
-	printf("Access granted");
-	return 0;
+#include <stdio.h>
+
+int main() {
+    int num, reversed = 0, original, remainder;
+
+    printf("Enter the number: ");
+    scanf("%d", &num);
+
+    original = num;
+
+    while (num != 0) {
+        remainder = num % 10;
+        reversed = reversed * 10 + remainder;
+        num = num / 10;
+    }
+
+    if (original == reversed) {
+        printf("The number is palindrome %d", original);
+    } else {
+        printf("The number is not palindrome %d", original);
+    }
+
+    return 0;
 }
