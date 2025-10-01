b6b748f15acaf871f4baa762913711b2efe6eb59
Muhammad Shuraim <k250733@nu.edu.pk>
2025-10-01 21:54:56 +0500

    Create CTask7.c

---
+++ CTask7.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    int original = 1234;
+    int user;
+
+    do {
+        printf("Enter the password: ");
+        scanf("%d", &user);
+    } while (user != original);
+
+    printf("Access granted");
+    return 0;
+}
