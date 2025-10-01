aa83b476c83b5ac9fa39e1ef0d8ebdcb44a29fd2
Muhammad Shuraim <k250733@nu.edu.pk>
2025-10-01 21:58:44 +0500

    Update CTask4.c

--- CTask4.c
+++ CTask4.c
@@ -1,15 +1,26 @@
-#include<stdio.h>
-int main(){
-	int original = 1234;
-	int user;
-	
-	
-	do{
-		printf("Enter the password: ");
-		scanf("%d" , &user);
-		
-	} while (user != original);
-	
-	printf("Access granted");
-	return 0;
+#include <stdio.h>
+
+int main() {
+    int n, i;
+    int f = 0, s = 1, next;
+
+    printf("Enter the number: ");
+    scanf("%d", &n);
+
+    printf("Fibonacci Series: ");
+
+    for (i = 0; i < n; i++) {
+        if (i == 0) {
+            printf("%d ", f);
+        } else if (i == 1) {
+            printf("%d ", s);
+        } else {
+            next = f + s;
+            printf("%d ", next);
+            f = s;
+            s = next;
+        }
+    }
+
+    return 0;
 }
